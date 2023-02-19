b5b7236fb8d4e22057bcf7e9a414bb90d015dbe0
Suraksha <surakshak571@gmail.com>
2023-02-20 01:48:43 +0500

    boxes added

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
-function App() {
+// function App() {
+//   return (
+//     <div className="App">
+      
+//     </div>
+//   );
+// }
+ function Box({value, onBoxClick}){
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <button onClick={onBoxClick}>{value}</button>
+  );
+}
+export default function Table() {
+  const [boxes, setBoxes] = useState(Array(9).fill(null));
+
+  function handleClick(i) {
+    const next = boxes.slice();
+    next[0] = "X";
+    setBoxes(next);
+  }
+  return (
+    <>
+      <div className="board-row">
+        <Box value={boxes[0]}/>
+        <Box value={boxes[1]}/>
+        <Box value={boxes[2]}/>
+      </div>
+      <div className="board-row">
+      <Box value={boxes[3]} />
+      <Box  value={boxes[4]}/>
+      <Box  value={boxes[5]}/>
+      </div>
+      <div className="board-row">
+      <Box  value={boxes[6]}/>
+      <Box  value={boxes[7]}/>
+      <Box  value={boxes[8]}/>
+      </div>
+    </>
   );
 }
 
-export default App;
